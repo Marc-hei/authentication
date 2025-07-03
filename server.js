35e52cd13c26bdfe040eab419f0a1248bee8bbc0
Gabriel Stegmaier <gabriel.stegmaier@bluewin.ch>
2025-07-03 17:03:17 +0200

    assertion added

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const expectedOrigin = ['http://localhost:3000'];
 // TODO: check database for username
 function registerStart (username) {
     let challenge = createUint8Array();
+    console.log("uuid: ", challenge.string);
+    console.log("challenge: ", challenge.array);
+
     let userID = createUint8Array();
-    challenges[username] = challenge;
+    challenges[username] = btoa(challenge.string);
     return {
-        challenge: challenge,
+        challenge: challenge.array,
         rp: {id: RPID, name: RPNAME},
-        user: {id: userID, name: username, displayName: username},
+        user: {id: userID.array, name: username, displayName: username},
         pubKeyCredParams: [
             {type: 'public-key', alg: -7},
             {type: 'public-key', alg: -8},
@@ -30,7 +33,39 @@ async function registerFinish (username, credential) {
     const attestation = credential.response;
     console.log(attestation.getPublicKey());
     console.log(attestation.getPublicKeyAlgorithm());
-    let verification;
+    const algoNum = attestation.getPublicKeyAlgorithm();
+    let algoName;
+    if (algoNum === -7) {
+        algoName = {name: 'ECDSA', hash: {name: 'SHA-256'}};
+    } else if (algoNum === -8) {
+        algoName = {name: 'Ed25519'};
+    } else if (algoNum === -257) {
+        algoName = {name: 'RSASSA-PKCS1-v1_5'}
+    } else {
+        return false;
+    }
+    // decode the clientDataJSON into a utf-8 string
+    const utf8Decoder = new TextDecoder('utf-8');
+    const decodedClientData = utf8Decoder.decode(
+        credential.response.clientDataJSON)
+
+    // parse the string as an object
+    const clientDataObj = JSON.parse(decodedClientData);
+
+    console.log("challenges[username]", challenges[username])
+    console.log("clientdataob.challenge", clientDataObj.challenge)
+    console.assert(clientDataObj.challenge === challenges[username], "failed")
+//    crypto.subtle.verify(
+//        algoName,
+//        attestation.getPublicKey(),
+//        attestation.attestationObject.attStmt.sig;
+
+
+    //console.dir(attestation.attestationObject);
+    //console.dir(attestation.clientDataJSON);
+    //console.dir(attestation);
+    //console.log(attestation.getTransports());
+    //console.log(attestation.getAuthenticatorData());
     // try {
     //     verification = await SimpleWebAuthnServer.verifyRegistrationResponse({
     //         response: credential.response,
@@ -94,8 +129,16 @@ async function loginFinish (username, credential) {
 };
 
 function createUint8Array() {
-  return crypto.getRandomValues(new Uint8Array(16));
+    const string = crypto.randomUUID();
+    const array = Uint8Array.from(string, c => c.charCodeAt(0));
+    console.log(string)
+    return {string, array};
 }
+
+//function createUint8Array() {
+//    return crypto.getRandomValues(new Uint8Array(16));
+//}
+    
 
 function transformUint8Array(arr) {
    return btoa(arr);
